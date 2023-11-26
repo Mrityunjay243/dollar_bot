83c965ecb8e333b53b5e1a19070950152c24d536
Atharva Thorve <aaathorve@gmail.com>
2023-11-26 15:26:36 -0500

    Updated support command

--- code/support.py
+++ code/support.py
@@ -7,5 +7,6 @@ def run(message, bot):
         "We'll reply right here as soon as we can.\n"
         "Phone: +19195597611\n"
         "Email: divitkalathil@gmail.com\n"
+        "Create Issues: https://github.com/Mrityunjay243/dollar_bot/issues\n"
     )
     bot.send_message(chat_id, support_message)
